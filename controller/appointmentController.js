6763a489c6105f86f6f65b2f13b9eb936bf25111
ericanthonywu <ericanthonywu89@gmail.com>
2020-01-12 10:28:16 +0700

    appointment reschedule, cancel and make scheduler push notif reminder

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -1,7 +1,8 @@
-const {appointment: Appointment,user: User} = require('../model')
+const {appointment: Appointment, vet: Vet} = require('../model')
 const moment = require('moment')
 const Axios = require('axios')
-const {vetPushNotif} = require("../globalHelper");
+const scheduler = require('node-schedule')
+const {userPushNotif, vetPushNotif} = require("../globalHelper");
 
 exports.addAppointment = (req, res) => {
     const {time, vet} = req.body
@@ -20,16 +21,20 @@ exports.addAppointment = (req, res) => {
                     time: time,
                     user: res.userData.id
                 }).save()
-                    .then(async () => {
+                    .then(async ({_id}) => {
                         res.status(200).json()
                         const {io} = req
-                        await vetPushNotif(vet,"Appointment baru",`Ada appointment baru dari user ${res.userData.username} pada tanggal ${moment(time).format("D MMMM pukul H:m")}`)
-                        io.sockets.emit('newAppointment',{
-                            to: vet,
-                            user: res.userData.username,
-                            time: time
+                        Vet.findById(vet).select("socketId username").then(({socketId, username}) => {
+                            scheduler.scheduleJob(_id, moment(time).subtract(15, "minutes").toISOString(), () => {
+                                vetPushNotif(vet, "Appointment Reminder", `15 menit lagi ada appoinment dengan user ${res.userData.username} jangan sampai telat ya!`)
+                                userPushNotif(res.userData.id, "Appointment Reminder", `15 menit lagi ada appoinment dengan Dr. ${username} jangan sampai telat ya!`)
+                            })
+                            io.sockets.connected[socketId].emit('newAppointment', {
+                                user: res.userData.username,
+                                time: time
+                            })
                         })
-
+                        await vetPushNotif(vet, "Appointment baru", `Ada appointment baru dari user ${res.userData.username} pada tanggal ${moment(time).format("D MMMM H:m")}`)
                     })
                     .catch(err => res.status(500).json(err))
             } else {
@@ -39,6 +44,24 @@ exports.addAppointment = (req, res) => {
     } else {
         return res.status(400).json()
     }
+}
+
+exports.reScheduleAppointment = (req, res) => {
+    const {id, time} = req.body
+    Appointment.findByIdAndUpdate(id, {
+        time: time
+    }).then(() => {
+        scheduler.scheduledJobs[id].reschedule(moment(time).subtract(15, "minutes").toISOString())
+    }).catch(err => res.status(500).json(err))
+}
+
+exports.cancelAppointment = (req, res) => {
+    const {id} = req.body
+    Appointment.findByIdAndDelete(id)
+        .then(() => {
+            scheduler.scheduledJobs[id].cancel()
+            res.status(200).json()
+        }).catch(err => res.status(500).json(err))
 }
 
 exports.showVetAvailable = (req, res) => {
